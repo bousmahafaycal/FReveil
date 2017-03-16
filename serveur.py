d322af7e597a4551e3dc6d1659228403b3b21a3b
Fayçal Bousmaha <faycal.bousmaha.fac@gmail.com>
2017-03-16 21:24:30 +0100

    Version 1.5.7.1 : Correction de bug

--- serveur.py
+++ serveur.py
@@ -82,12 +82,12 @@ class Serveur:
         
         elif Outils.recupereBaliseAuto(message,"c",1) == "getCommande":
             c = Config()
-            print("getCommande detecté")
+            #print("getCommande detecté")
             liste = []
-            print("liste")
+            #print("liste")
             for i in range (len(c.listeModule)):
-                liste.append(c.listeModule[0])
-            print ("liste : "+str(liste))
+                liste.append(c.listeModule[i][0])
+            #print ("liste : "+str(liste))
             chaine = self.listToString(c.liste)
 
         elif Outils.recupereBaliseAuto(message,"c",1) == "getRappelJournalier":
